4cd33ca48fae10479c9de87eb0936ba2af85ce35
Harsh Shiroya <115132103+HarshShiroya@users.noreply.github.com>
2024-01-28 13:07:32 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+# This is a simple Python script
+name = input("What is your name? ") # Get the user's name
+age = int(input("How old are you? ")) # Get the user's age and convert it to an integer
+print("Hello, " + name + "!") # Print a greeting message
+print("You are " + str(age) + " years old.") # Print the user's age and convert it to a string
